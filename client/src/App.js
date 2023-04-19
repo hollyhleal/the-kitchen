773c6499b48ccd3310467a1503cb868154922a81
CWhite2127 <cwhitewesterly2@gmail.com>
2023-04-19 17:04:51 -0600

    updates home page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'
+import Footer from './components/Footer'
 
 //import pages
 
