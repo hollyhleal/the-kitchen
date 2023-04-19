3fc5ff4c45676e3e752ed06b1bd3efd1ecad26c9
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-19 15:24:55 -0600

    add routes for profile and booking and update to profile layout

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -12,21 +12,19 @@ function Profile() {
       <div class="grid grid-rows-3 grid-flow-col gap-4">
         {/* left-hand side column for profile photo/bio */}
         <div class="row-span-3">
-          <h2>Welcome back, !</h2>
+          <h2>Welcome back, Name!</h2>
           <Card className="w-96">
             <CardHeader floated={false} className="h-80">
               <img src="" alt="profile-picture" />
             </CardHeader>
             <CardBody className="text-center">
               <Typography variant="h4" color="blue-gray" className="mb-2">
-                
+                Name
               </Typography>
               <Typography color="blue" className="font-medium" textGradient>
-                
+                Level
               </Typography>
-              <Typography className="font-medium">
-                Member Since: 
-              </Typography>
+              <Typography className="font-medium">Member Since:</Typography>
             </CardBody>
           </Card>
         </div>
