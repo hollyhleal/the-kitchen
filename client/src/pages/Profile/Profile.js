da9b8bf3f527a8471f4382925184a2e52d986ba2
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-19 13:44:02 -0600

    update profile to include all card and row components

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,67 +1,44 @@
 import {
+  Button,
   Card,
   CardHeader,
   CardBody,
-  CardFooter,
   Typography,
-  Tooltip,
 } from "@material-tailwind/react";
 
 function Profile() {
   return (
     <>
-      <div className="columns-3">
-        <Card className="w-96">
-          <CardHeader floated={false} className="h-80">
-            <img src="/img/team-3.jpg" alt="profile-picture" />
-          </CardHeader>
-          <CardBody className="text-center">
-            <Typography variant="h4" color="blue-gray" className="mb-2">
-              Natalie Paisley
-            </Typography>
-            <Typography color="blue" className="font-medium" textGradient>
-              CEO / Co-Founder
-            </Typography>
-          </CardBody>
-          <CardFooter className="flex justify-center gap-7 pt-2">
-            <Tooltip content="Like">
-              <Typography
-                as="a"
-                href="#facebook"
-                variant="lead"
-                color="blue"
-                textGradient
-              >
-                <i className="fab fa-facebook" />
+      <div class="grid grid-rows-3 grid-flow-col gap-4">
+        {/* left-hand side column for profile photo/bio */}
+        <div class="row-span-3">
+          <h2>Welcome back, {user}!</h2>
+          <Card className="w-96">
+            <CardHeader floated={false} className="h-80">
+              <img src={image} alt="profile-picture" />
+            </CardHeader>
+            <CardBody className="text-center">
+              <Typography variant="h4" color="blue-gray" className="mb-2">
+                {user}
               </Typography>
-            </Tooltip>
-            <Tooltip content="Follow">
-              <Typography
-                as="a"
-                href="#twitter"
-                variant="lead"
-                color="light-blue"
-                textGradient
-              >
-                <i className="fab fa-twitter" />
+              <Typography color="blue" className="font-medium" textGradient>
+                {Level}
               </Typography>
-            </Tooltip>
-            <Tooltip content="Follow">
-              <Typography
-                as="a"
-                href="#instagram"
-                variant="lead"
-                color="purple"
-                textGradient
-              >
-                <i className="fab fa-instagram" />
+              <Typography className="font-medium">
+                Member Since: {Year}
               </Typography>
-            </Tooltip>
-          </CardFooter>
-        </Card>
-      </div>
+            </CardBody>
+          </Card>
+        </div>
 
-      <div className="columns-9"></div>
+        <div class="col-span-2">
+          <Button size="lg">Book a Court</Button>
+        </div>
+
+        <div class="row-span-2 col-span-2">
+          <h3>Upcoming Reservations</h3>
+        </div>
+      </div>
     </>
   );
 }
