654e4e902d8d30425fc9c14200bfbc0ebb9bb8e4
CWhite2127 <cwhitewesterly2@gmail.com>
2023-04-20 13:18:54 -0600

    updates home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -19,15 +19,25 @@ export default function Home() {
   return (
     <>
       <div
-        className=" bg-opacity-100 bg-cover bg-center h-96 bg-no-repeat content"
-        style={{ backgroundImage: `url(${Paddle})` }}
-      ></div>
-      <div className="container mx-auto grid grid-cols-3 h-96">
-        <div className="place-self-center">test</div>
+        className=" opacity-80 bg-cover bg-center h-96 bg-no-repeat content"
+        style={{ backgroundImage: `url(${Paddle})` }}>
+          <h3 class="absolute text-5xl text-white text-left pr-60 top-1/4 left-1/4 -translate-x-1/2 -translate-y-1/2">
+            You Can't Serve Up </h3>
+            <h3 class="absolute text-5xl text-white pl-6 contrast-125 top-1/3 left-1/4 -translate-x-1/2 -translate-y-1/2">
+            A Masterpiece If You're Not In... </h3>
+            <h1 class="absolute text-8xl text-white pl-6 font-bold contrast-125 top-1/2 right-1/4 -translate-x-1/2 -translate-y-1/2">
+            The Kitchen </h1>
+        </div>
+      <div className="container mx-auto grid grid-cols-3 h-96 text-center">
         <div className="place-self-center">test</div>
         <div className="place-self-center">
+        <Typography variant="h2">What is 'The Kitchen?'</Typography>
+            <p className="pt-6 text-xl">On a pickleball court, there is a seven-foot non-volley zone that extends from the net on both sides of the court. The term “kitchen” refers to this non-volley zone.</p>
+            <p className="pt-6 text-xl">Here however, the Kitchen is your best way of reserving a pickleball court for you and your friends to "Dink", "Erne", and "Volley"</p>
+        </div>
+        <div className="place-self-center">
           <React.Fragment>
-            <Button onClick={handleOpen}>Sign In</Button>
+            <Button size="lg" onClick={handleOpen}>Sign In</Button>
             <Dialog
               size="xs"
               open={open}
