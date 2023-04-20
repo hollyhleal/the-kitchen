a0fe115ccb0b67c254e516e9868c24cb0d77cd43
CWhite2127 <cwhitewesterly2@gmail.com>
2023-04-20 15:12:18 -0600

    updates home page styling and adds color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = withMT({
     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
+    colors: {
+      'teal': '#095256',
+      'green': '#849324',
+      'beige': '#F5F7DC',
+      'orchid': '#998DA0',
+      'blue': '#125E8A',
+    },
     extend: {},
   },
   plugins: [],
