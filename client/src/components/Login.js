685c92d8d6f907dc52b1540a9f599137ae6f7131
Holly Leal <94670410+hollyhleal@users.noreply.github.com>
2023-04-22 12:47:17 -0600

    update to remove signup modal and add signup form to home.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -42,9 +42,6 @@ const Login = () => {
 
   return (
     <>
-      {/* <Button className="hidden lg:inline-block" onClick={handleOpen}>
-  Sign In
-</Button> */}
       <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
