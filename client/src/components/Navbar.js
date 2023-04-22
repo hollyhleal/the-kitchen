fac13a2d36c44999ff70d125f7484662b043ebef
CWhite2127 <cwhitewesterly2@gmail.com>
2023-04-22 15:09:17 -0600

    updates NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -106,57 +106,6 @@ export default function NavBar() {
           </Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
-            <React.Fragment>
-              <Button className="hidden lg:inline-block" onClick={handleOpen}>
-                Sign In
-              </Button>
-              <Dialog
-                size="xs"
-                open={open}
-                handler={handleOpen}
-                className="bg-transparent shadow-none"
-              >
-                <Card className="mx-auto w-full max-w-[24rem]">
-                  <CardHeader
-                    variant="gradient"
-                    color="blue"
-                    className="mb-4 grid h-28 place-items-center"
-                  >
-                    <Typography variant="h3" color="white">
-                      Sign In
-                    </Typography>
-                  </CardHeader>
-                  <CardBody className="flex flex-col gap-4">
-                    <Input label="Email" size="lg" />
-                    <Input label="Password" size="lg" />
-                    <div className="-ml-2.5">
-                      <Checkbox label="Remember Me" />
-                    </div>
-                  </CardBody>
-                  <CardFooter className="pt-0">
-                    <Button variant="gradient" showModal={showSignupModal} setShowModal={setShowSignupModal} onClick={handleOpen} fullWidth>
-                      Sign In
-                    </Button>
-                    
-                    <Typography
-                      variant="small"
-                      className="mt-6 flex justify-center"
-                    >
-                      Don&apos;t have an account?
-                      <Typography
-                        as="button"
-                        variant="small"
-                        color="blue"
-                        className="ml-1 font-bold"
-                        onClick={handleSignupOpen}
-                      >
-                        Sign up 
-                      </Typography>
-                    </Typography>
-                  </CardFooter>
-                </Card>
-              </Dialog>
-            </React.Fragment>
             <IconButton
               variant="text"
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
